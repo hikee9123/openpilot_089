f3fb2472549835e8ba844a2fb58db3ef08532db7
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2021-09-14 09:40:58 +0900

    Update driver_monitor.py

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -29,15 +29,15 @@ class DRIVER_MONITOR_SETTINGS():
 
     self._FACE_THRESHOLD = 0.5
     self._PARTIAL_FACE_THRESHOLD = 0.765 if TICI else 0.455
-    self._EYE_THRESHOLD = 0.25 if TICI else 0.57
+    self._EYE_THRESHOLD = 0.25 if TICI else 0.45
     self._SG_THRESHOLD = 0.83
-    self._BLINK_THRESHOLD = 0.62 if TICI else 0.68
+    self._BLINK_THRESHOLD = 0.62 if TICI else 0.35 # 0.68
     self._BLINK_THRESHOLD_SLACK = 0.82 if TICI else 0.88
     self._BLINK_THRESHOLD_STRICT = 0.62 if TICI else 0.68
     self._PITCH_WEIGHT = 1.175 if TICI else 1.35  # pitch matters a lot more
     self._POSESTD_THRESHOLD = 0.2 if TICI else 0.175
     self._E2E_POSE_THRESHOLD = 0.95 if TICI else 0.9
-    self._E2E_EYES_THRESHOLD = 0.75
+    self._E2E_EYES_THRESHOLD = 0.45  # 0.75
 
     self._METRIC_THRESHOLD = 0.55 if TICI else 0.48
     self._METRIC_THRESHOLD_SLACK = 0.75 if TICI else 0.66
